bbc87540d3cb1a6b3d668a7338965a4c81454358
Vadim Trusov <o7arm@yandex.ru>
2022-07-11 18:53:39 +0300

    dev model

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,17 +1,26 @@
 from re import search
 from django.contrib import admin
-from .models import Post
+from .models import Group, Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+        )
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-# При регистрации модели Post источником конфигурации для неё назначаем 
-# класс PostAdmin
+    # При регистрации модели Post источником конфигурации для неё назначаем 
+    # класс PostAdmin
+    list_editable = ('group',)
+
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,16 +5,6 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
-
 class Group(models.Model):
     title = models.TextField()
     slug = models.SlugField(unique=True)
@@ -22,3 +12,20 @@ class Group(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
